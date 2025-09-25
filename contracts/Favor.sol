4fbc45079dc3b16a068c059d0bb4e6172bc2b150
grapefi <grapefinance@protonmail.com>
2025-09-25 17:16:05 +0200

    clean up now redundant tax logic in favor _update

--- contracts/Favor.sol
+++ contracts/Favor.sol
@@ -11,14 +11,11 @@ import {IFavorToken} from "./interfaces/IFavorToken.sol";
  * base favor contract  with common logic
  */
 
-// TODO: Check contract transfer revert implementation and clean up tax logic
-
 contract Favor is IFavorToken, ERC20Burnable, Ownable2Step {
 
     uint256 public constant MULTIPLIER = 10000;
     uint256 public constant MAX_TAX = 5000; // 50% MAX Sell Tax
     uint256 public constant BONUS_TWAP_THRESHOLD = 3e18;
-    //uint256 public constant CONTRACT_TAX = 9900; // Tax collected in Favor for sending to non whitelisted contracts
     
     uint256 public sellTax = 5000;
     uint256 public bonusRate = 4400; // Buy bonus to buyer in esteem
@@ -99,9 +96,9 @@ contract Favor is IFavorToken, ERC20Burnable, Ownable2Step {
 
     /**
      * @dev See {IERC20-_update}.
-     * here we apply taxation.  if destination is a contract, we treat is as a sell.
-     * edge cases like buy via approved contracts  while minting  ersteem of whatever
-     * will be tax exempt.
+     * here we apply transfer rules.  if destination is a contract, we revert the transfer.
+     * edge cases like buy via approved contracts  while minting  esteem or whatever
+     * will be tax exempt and allowed.
      */
     function _update(address _from, address _to, uint256 _value) internal override {
         bool destinationIsContract = _to.code.length != 0;
@@ -111,20 +108,11 @@ contract Favor is IFavorToken, ERC20Burnable, Ownable2Step {
             super._update(_from, _to, _value);
             return;
         }
-
-        //uint256 taxAmount = 0;
         
         if (destinationIsContract) {
             revert("Transfer not allowed");
-            //taxAmount = (_value * CONTRACT_TAX) / MULTIPLIER;
         }
 
-        /*
-        if (taxAmount > 0) {
-            super._update(_from, treasury, taxAmount);
-            _value -= taxAmount;
-        }
-        */
         super._update(_from, _to, _value);
     }
 
